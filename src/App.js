5105287dbd792d89bb40d21643d05027ec433122
rastifar <rastifar@yahoo.com>
2024-10-10 04:05:18 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-
-import Home from './pages/Home';
-import Product from './pages/Product';
-import ProductList from './pages/ProductsList';
-import Register from './pages/Register';
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductsList";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
-      {/* <Home/> */}
-      {/* <ProductList/> */}
-      {/* <Product/> */}
-      <Register/>
-     
+      <Home />
+      <ProductList />
+      <Product />
+      <Register />
     </div>
   );
 }
